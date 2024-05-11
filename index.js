41d74ad82685bc2da8501e897846411095d5b41d
Muhammad Shahzil <mshahzil56@gmail.com>
2024-05-11 16:33:09 +0500

    SIMPLE CALCULATER

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+// Asking q from user to throgh inquirer
+let answers = await inquirer.prompt([
+    { message: "ENTER YOUR FIRST NUMBER", type: "number", name: "FirstNumber", },
+    { message: "ENTER YOUR SECOND NUMBER", type: "number", name: "SecondNumber" },
+    {
+        message: "SELECT ONE OPERATER TO PERFORM OPERATIONS",
+        type: "list",
+        name: "operater",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    }
+]);
+console.log(answers);
+// Else if statement use 
+if (answers.operater === "ADDITION") {
+    console.log(answers.FirstNumber + answers.SecondNumber);
+}
+else if (answers.operater === "SUBTRACTION") {
+    console.log(answers.FirstNumber - answers.SecondNumber);
+}
+else if (answers.operater === "MULTIPLICATION") {
+    console.log(answers.FirstNumber * answers.SecondNumber);
+}
+else if (answers.operater === "DIVISION") {
+    console.log(answers.FirstNumber / answers.SecondNumber);
+}
+else {
+    console.log("YOU TYPE A INVALID output!");
+}
